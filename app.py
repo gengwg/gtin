9b0be823c0a038223a4f67c44948e3e2ced250a4
Weigang Geng <gengwg@gmail.com>
2019-03-26 18:41:41 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, abort, make_response, request
+from gtin import GTIN
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/gtin/api/v1.0/convert', methods=['POST'])
+def convert_upc_to_gtin():
+    if not request.json or not 'upc' in request.json:
+        abort(400)
+    upc = request.json['upc']
+    print (request.json)
+    length = request.json['length'] if 'length' in request.json else 12
+    res = str(GTIN(raw=request.json['upc'], length=length))
+
+    return jsonify({'gtin': res}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
